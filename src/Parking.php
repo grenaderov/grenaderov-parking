7a3d8b90af98830ecac698f83500e4a9802b474d
Alexander Grenaderov <amgreen@mail.ru>
2022-01-25 17:58:36 +0300

    #6 add exception

--- src/Parking.php
+++ src/Parking.php
@@ -4,8 +4,17 @@ namespace App;
 
 class Parking
 {
-    public function __construct(private int $capacity)
+    public function __construct(private $capacity)
     {
+        if (!is_int($capacity)) {
+            throw new DomainException('Число должно быть целым');
+        }
+        if ($capacity === 0) {
+            throw new DomainException('Парковка не может быть без мест');
+        }
+        if ($capacity < 0) {
+            throw new DomainException('Укажите положительное количество мест');
+        }
     }
 
     public function getCapacity(): int
