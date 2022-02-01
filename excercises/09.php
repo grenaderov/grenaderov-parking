3b7cc294f000b5984a00122291c0748540c3c26e
Alexander Grenaderov <amgreen@mail.ru>
2022-02-02 00:47:13 +0300

    #16 transfer repository to api

--- excercises/09.php
+++ excercises/09.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Api;
+use App\Repository;
 
-$api = new Api();
+$api = new Api(new Repository());
 $parking = $api->createParking(4);
+
--- src/Api.php
+++ src/Api.php
@@ -3,16 +3,17 @@
 namespace App;
 
 use App\Parking\Parking;
-use App\Repository;
 
 class Api
 {
+    public function __construct(private Repository $repo)
+    {
+    }
+
     public function createParking(int $parkingSize): Parking
     {
         $parking = new Parking($parkingSize);
-
-        $repo = new Repository();
-        $repo->save($parking);
+        $this->repo->save($parking);
 
         return $parking;
     }
